438eb1e7e89d78843f413733544441e5da97d0aa
JRMendDemo <122036228+JRMendDemo@users.noreply.github.com>
2024-05-08 11:44:42 -0500

    Update feedback.js

--- src/main/resources/lessons/csrf/js/feedback.js
+++ src/main/resources/lessons/csrf/js/feedback.js
@@ -1,4 +1,5 @@
 webgoat.customjs.feedback = function() {
+    var test = "test";
     var data = {};
     $('#csrf-feedback').find('input, textarea, select').each(function(i, field) {
         data[field.name] = field.value;
